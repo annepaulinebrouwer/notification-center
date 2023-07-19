8691a6d6840362f4a6ce767f45e0f5ce8900f301
Paula Brouwer <freelance.brouwer@gmail.com>
2023-07-19 15:27:40 +0200

    Set up the API structure and build get api/v1/notifications/:id endpoint

---
+++ app/controllers/api/v1/notifications_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::NotificationsController < ApplicationController
+  before_action :set_notification, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def set_notification
+    @notification = Notification.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :notifications, only: [:show]
+    end
+  end
 end
