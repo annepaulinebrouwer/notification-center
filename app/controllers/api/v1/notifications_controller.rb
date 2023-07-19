b5a1bac3f67d4505714b170c09360a4dd619b859
Paula Brouwer <freelance.brouwer@gmail.com>
2023-07-19 15:48:55 +0200

    Add endpoint to create a notification to the API

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,12 +1,33 @@
 class Api::V1::NotificationsController < ApplicationController
   before_action :set_notification, only: [:show]
+  acts_as_token_authentication_handler_for User, only: [:create]
 
   def show
   end
 
+  def create
+    return render json: { status: :unauthorized } unless current_user.admin?
+
+    @notification = Notification.new(notification_params)
+    if @notification.save
+      render :show
+    else
+      render_error
+    end
+  end
+
   private
+
+  def notification_params
+    params.require(:notification).permit(:date, :title, :description)
+  end
 
   def set_notification
     @notification = Notification.find(params[:id])
+  end
+
+  def render_error
+    render json: { errors: @notification.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
