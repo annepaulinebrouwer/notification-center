9fd237ec8fe4d7395e3b51b2b129c257cc3b5b2b
Paula Brouwer <freelance.brouwer@gmail.com>
2023-07-19 14:15:51 +0200

    Settig up the user model with devise and simple_token_authentication

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 
+gem "devise"
+gem "simple_token_authentication"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
@@ -45,4 +48,3 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+User.destroy_all
+
+puts "Creating admin user"
+User.create(
+  email: "admin@inyova.com",
+  password: "amazing_password",
+  admin: true
+)
+
+puts "Creating two clients"
+User.create(
+  email: "paula@gmail.com",
+  password: "testtest"
+)
+
+User.create(
+  email: "anne@gmail.com",
+  password: "test1234"
+)
+
+puts "Finished"
